7cf2358c902b318f19bebb978a62cc5876220195
Yutaro Sakamoto <y-sakamoto@tsh-world.co.jp>
2021-11-30 09:45:21 +0900

    fix: issue #15

--- dblibj/src/main/scala/Operation.scala
+++ dblibj/src/main/scala/Operation.scala
@@ -2,10 +2,8 @@ import cats.data.ContT
 import cats.free.Free
 import cats.free.Free.liftF
 import cats.{Id, ~>}
-import ConnectionInfo._
 import cats.data.StateT
 
-import java.io._
 import java.sql._
 import java.util.Properties
 import scala.collection.immutable.Queue
@@ -113,8 +111,8 @@ object Operation {
             }
           }
           case Exec(addr, query) => {
-            val stmt = addr.createStatement()
             try {
+              val stmt = addr.createStatement()
               if(stmt.execute(query)) {
                 Right(EResultSet(stmt.getResultSet()))
               } else {
@@ -127,11 +125,11 @@ object Operation {
             }
           }
           case ExecParam(addr, query, params) => {
-            val stmt = addr.prepareStatement(query)
-            for ((param, i) <- params.zipWithIndex) {
-              param.setParam(stmt, i + 1)
-            }
             try {
+              val stmt = addr.prepareStatement(query)
+              for ((param, i) <- params.zipWithIndex) {
+                param.setParam(stmt, i + 1)
+              }
               if(stmt.execute()) {
                 Right(EResultSet(stmt.getResultSet()))
               } else {
