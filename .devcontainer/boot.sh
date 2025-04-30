e83c04ba1e7e777db0db571fd583f9598a44cf61
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-04-30 02:10:20 +0000

    fix: settings of Dev Containers

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Install opensource COBOL 4J
+git submodule init
+git submodule update
 cd opensourcecobol4j/
 ./configure --prefix=/usr/
 make
@@ -9,19 +11,9 @@ cd ../
 
 # Install Open COBOL ESQL 4J
 cp /usr/lib/opensourcecobol4j/libcobj.jar dblibj/lib/
-cp /usr/lib/opensourcecobol4j/libcobj.jar dblibj/lib/
 mkdir -p /usr/lib/Open-COBOL-ESQL-4j
 curl -L -o /usr/lib/Open-COBOL-ESQL-4j/postgresql.jar https://jdbc.postgresql.org/download/postgresql-42.2.24.jar
 cp /usr/lib/Open-COBOL-ESQL-4j/postgresql.jar dblibj/lib/
 ./configure --prefix=/usr/
 make
 make install
-
-# Set up git config
-git config --global --add safe.directory /workspaces/Open-COBOL-ESQL-4j
-
-# Set up pre-commit hook
-cp .devcontainer/term_settings/pre-commit .git/hooks/pre-commit
-
-# Set up ~/.bashrc
-cat .devcontainer/term_settings/extra_bashrc.sh >> ~/.bashrc
